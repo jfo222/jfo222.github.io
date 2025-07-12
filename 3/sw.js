21375359452c514906c3db05af2f0b93023af71a
JFO <190934764+jfo222@users.noreply.github.com>
2025-07-12 02:11:55 +0200

    ***

---
+++ 3/sw.js
@@ -0,0 +1,33 @@
+const id = "#3:25-07-13";
+const shell = [
+	"./",
+	"./config.json",
+	"./font.woff2",
+	"./icon.svg",
+	"./lang/en.json",
+	"./manifest.json",
+	"./script.mjs",
+	"./style.css"
+];
+self.addEventListener("install", (e) => e.waitUntil(
+	caches.open(id).then(
+		(c) => c.addAll(shell)
+	)
+));
+self.addEventListener("activate", (e) => e.waitUntil(
+	caches.keys().then(
+		(k) => Promise.all(
+			k.map(function (key) {
+				if (key.includes("#3") && key !== id) {
+					return caches.delete(key);
+				}
+			})
+		)
+	)
+));
+self.addEventListener("fetch", (e) => e.respondWith(
+	caches.match(e.request, {ignoreVary: true}).then(
+		(r) => r || fetch(e.request)
+	)
+));
+
