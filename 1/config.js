0527155535093594612e201375fe2b76e30aeafd
JFO <190934764+jfo222@users.noreply.github.com>
2026-04-28 10:36:11 +0200

    ***

---
+++ 1/config.js
@@ -0,0 +1,21 @@
+globalThis.CONFIG = {
+	alphanumeric: "numeric",
+	limitMax: 9,
+	limitMin: 5,
+	nightMode: false,
+	randomize: {
+		arrow: true,
+		char: true
+	},
+	reverse: {
+		arrow: "alternate",
+		char: false
+	},
+	sound: true,
+	time: {
+		0: 0,
+		1: 1500,
+		2: 210
+	}
+}
+
